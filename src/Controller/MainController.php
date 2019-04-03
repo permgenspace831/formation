e0e800196ca5e7852368e5dfd17b3b1e517b5a06
Benoît Tavernier <benoit.tavernier@gmail.com>
2019-04-03 08:40:49 +0200

    Initial SF 4.2 project with 2 empty controllers

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="homepage", methods={"GET"})
+     */
+    public function homepage(): Response
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact", methods={"GET", "POST"})
+     */
+    public function contact(): Response
+    {
+
+        return $this->render('main/contact.html.twig');
+    }
+}
